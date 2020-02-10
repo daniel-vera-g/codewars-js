bd8ed231251cff24280e691ab02ab0668f2407fc
danielVg <danielveragi@gmail.com>
2020-02-10 08:41:32 +0100

    Small refactoring to have less code

--- kyu6/createPhoneNumber.js
+++ kyu6/createPhoneNumber.js
@@ -1,6 +1,6 @@
 module.exports = function createPhoneNumber(numbers) {
-	let phoneNumber = null;
-
-	phoneNumber = "(" + numbers.slice(0,3).join("") + ")" + " " + numbers.slice(3,6).join("") + "-" + numbers.slice(6,10).join("");
-	return phoneNumber;
+	// Use .slice to get each part of the phone number from the array
+	return (
+		"(" + numbers.slice(0, 3).join("") + ")" + " " + numbers.slice(3, 6).join("") + "-" + numbers.slice(6, 10).join("")
+	);
 };

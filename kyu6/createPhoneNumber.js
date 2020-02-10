462175f0fd4c1457971aad110eeb32eb9d878b14
danielVg <danielveragi@gmail.com>
2020-02-10 08:50:50 +0100

    Added missing link to kata

--- kyu6/createPhoneNumber.js
+++ kyu6/createPhoneNumber.js
@@ -1,3 +1,4 @@
+// https://www.codewars.com/kata/525f50e3b73515a6db000b83
 module.exports = function createPhoneNumber(numbers) {
 	// Use .slice to get each part of the phone number from the array
 	return (
